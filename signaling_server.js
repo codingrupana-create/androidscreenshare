83a34e10a65221ced81e0f1bba291621fd2e5e87
codingrupana-create <codingrupana@gmail.com>
2026-05-03 13:28:47 +0530

    Add files via upload

---
+++ signaling_server.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: "*" }
+});
+
+const PORT = process.env.PORT || 3000;
+const rooms = {};
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("join", (roomId) => {
+    socket.join(roomId);
+    console.log(`User ${socket.id} joined room ${roomId}`);
+    socket.to(roomId).emit("user_joined");
+  });
+
+  socket.on("set_pin", (data) => {
+    const { roomId, pin } = data;
+    rooms[roomId] = pin;
+    console.log(`PIN set for room ${roomId}: ${pin}`);
+  });
+
+  socket.on("verify_pin", (data) => {
+    const { roomId, pin } = data;
+    const isValid = rooms[roomId] === pin;
+    console.log(`PIN verification for room ${roomId}: ${pin} -> ${isValid}`);
+    socket.emit("pin_validation", isValid);
+  });
+
+  socket.on("ready_for_offer", (roomId) => {
+    console.log(`Remote ready for offer in room ${roomId}`);
+    socket.to(roomId).emit("ready_for_offer");
+  });
+
+  socket.on("offer", (data) => {
+    socket.to(data.roomId).emit("offer", data);
+  });
+
+  socket.on("answer", (data) => {
+    socket.to(data.roomId).emit("answer", data);
+  });
+
+  socket.on("ice_candidate", (data) => {
+    socket.to(data.roomId).emit("ice_candidate", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Signaling server running on port ${PORT}`);
+});
